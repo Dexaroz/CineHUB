e3abf2a5860bf45ce60c7562b34d237084f0e3fe
dexaroz <edumarreroglezz@gmail.com>
2025-11-09 19:38:40 +0000

    cors act

--- app/main.py
+++ app/main.py
@@ -36,8 +36,8 @@ def create_app() -> Flask:
     @app.after_request
     def add_cors_headers(response):
         response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type,X-Api-Key'
-        response.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type', 'Accept', 'x-api-key'
+        response.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,PATCH,DELETE,OPTIONS'
         return response
 
     @app.route('/<path:path>', methods=['OPTIONS'])
